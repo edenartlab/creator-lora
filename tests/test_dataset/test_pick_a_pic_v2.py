102063824517aa7d234a82fd6a6c613304a5d660
mayukh <mayukhmainak2000@gmail.com>
2023-12-18 00:41:50 -0800

    test download and append

---
+++ tests/test_dataset/test_pick_a_pic_v2.py
@@ -0,0 +1,38 @@
+import pytest
+import os
+from creator_lora.dataset import PickAPicV2Subset
+
+parquet_urls = [
+    {
+        "url": "https://huggingface.co/datasets/yuvalkirstain/pickapic_v2/resolve/main/data/test-00000-of-00014-387db523fa7e7121.parquet",
+        "filename": "test.parquet"
+    },
+    {
+        "url": "https://huggingface.co/datasets/yuvalkirstain/pickapic_v2/resolve/main/data/test-00001-of-00014-b4d27779c32b8591.parquet",
+        "filename": "test_2.parquet"
+    },
+]
+
+parquet_filename = "test.parquet"
+parquet_filename_2 = "test_2.parquet"
+
+def run_cleanup():
+    os.system(f"rm {parquet_filename}")
+    os.system(f"rm {parquet_filename_2}")
+
+@pytest.mark.parametrize("url", parquet_urls)
+def test_download(url: str):
+    dataset = PickAPicV2Subset.from_url(url = url, parquet_filename=parquet_filename)
+
+    assert len(dataset) > 0
+    run_cleanup()
+
+def test_append():
+
+    dataset = PickAPicV2Subset.from_url(url = parquet_urls[0]["url"], parquet_filename=parquet_urls[0]["filename"])
+    original_len_dataset = len(dataset)
+    dataset_2 = PickAPicV2Subset.from_url(url = parquet_urls[1]["url"], parquet_filename=parquet_urls[1]["filename"])
+
+    dataset.append(dataset_2)
+    assert len(dataset) == original_len_dataset + len(dataset_2)
+    run_cleanup()
