18f503f1c9a58fcf8e23289a801c8b4b48cdde90
mayukh <mayukhmainak2000@gmail.com>
2023-12-20 10:07:58 -0800

    save pil images for each uid

--- generate_clip_embeddings.py
+++ generate_clip_embeddings.py
@@ -5,10 +5,11 @@ from creator_lora.dataset import (
 )
 import os
 from creator_lora.utils import create_new_clean_folder
+from tqdm import tqdm
 
 device = "cuda:0"
 
-for i in range(0, 13):
+for i in tqdm(range(0, 35), desc = "Loading data"):
     parquet_filename = os.path.join("downloaded_dataset", f"{i}.pth")
     if i == 0:
         dataset = PickAPicV2Subset(
@@ -25,35 +26,59 @@ user_context_dataset = UserContextDataset(pick_a_pic_v2_subset=dataset)
 
 import numpy as np
 
-image_0_uids = np.unique(dataset.pandas_dataframe.image_0_uid.values)
-image_1_uids = np.unique(dataset.pandas_dataframe.image_1_uid.values)
+def save_all_unique_images_from_pick_a_pic_v2_subset(pick_a_pic_v2_subset: PickAPicV2Subset, output_folder: str, skip_if_exists: bool):
+    image_0_uids = np.unique(pick_a_pic_v2_subset.pandas_dataframe.image_0_uid.values)
+    image_1_uids = np.unique(pick_a_pic_v2_subset.pandas_dataframe.image_1_uid.values)
 
-pil_images = []
-uids = []
+    uids = []
+    filenames = []
+    for uid in tqdm(image_0_uids, desc = "saving pil images [image_0]"):
+        
+        if uid not in uids:
+            filename = os.path.join(output_folder, f"{uid}.jpg")
+            filenames.append(filename)
+            if os.path.exists(filename) and skip_if_exists:
+                continue
+            else:
+                dataset.get_image_from_image_0_uid(image_0_uid=uid).save(
+                    filename
+                )
+            uids.append(uid)
 
-for uid in image_0_uids:
-    if uid not in uids:
-        pil_images.append(dataset.get_image_from_image_0_uid(image_0_uid=uid))
-        uids.append(uid)
+    for uid in tqdm(image_1_uids, desc = "saving pil images [image_1]"):
+        
+        if uid not in uids:
+            filename = os.path.join(output_folder, f"{uid}.jpg")
+            filenames.append(filename)
+            if os.path.exists(filename) and skip_if_exists:
+                continue
+            else:
+                dataset.get_image_from_image_1_uid(image_1_uid=uid).save(
+                    filename
+                )
+            uids.append(uid)
 
-for uid in image_1_uids:
-    if uid not in uids:
-        pil_images.append(dataset.get_image_from_image_1_uid(image_1_uid=uid))
-        uids.append(uid)
+    return filenames, uids
 
-image_encoder = CLIPImageEncoder(name="RN50", device=device)
+image_paths, uids = save_all_unique_images_from_pick_a_pic_v2_subset(
+    pick_a_pic_v2_subset=dataset,
+    output_folder="pick_a_pic_images",
+    skip_if_exists=True
+)
+
+image_encoder = CLIPImageEncoder(name='ViT-B/32', device=device)
 # ## TODO: encode images based on image uid and not dataset index
 
 image_embeddings_folder = "./clip_embeddings"
 clip_embeddings_filenames = [
     os.path.join(image_embeddings_folder, f"{uids[count]}.pth")
-    for count in range(len(pil_images))
+    for count in range(len(image_paths))
 ]
 
-# create_new_clean_folder(image_embeddings_folder)
+create_new_clean_folder(image_embeddings_folder)
 
 image_encoder.encode_and_save_batchwise(
-    pil_images=pil_images,
+    image_paths=image_paths,
     output_filenames=clip_embeddings_filenames,
     batch_size=256,
     skip_if_exists=True
