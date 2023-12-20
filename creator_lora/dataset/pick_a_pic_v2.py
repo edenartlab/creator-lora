140de6f535a42b4856b425c0a611ad3ddb26c080
mayukh <mayukhmainak2000@gmail.com>
2023-12-20 02:02:16 -0800

    build UserContextDataset + formatting

--- creator_lora/dataset/pick_a_pic_v2.py
+++ creator_lora/dataset/pick_a_pic_v2.py
@@ -4,6 +4,8 @@ from PIL import Image
 import io
 from typing import List
 from fastparquet import write
+import numpy as np
+
 
 class PickAPicV2Subset:
     """
@@ -11,21 +13,28 @@ class PickAPicV2Subset:
 
     Example URL: https://huggingface.co/datasets/yuvalkirstain/pickapic_v2/resolve/main/data/test-00000-of-00014-387db523fa7e7121.parquet
     """
+
     def __init__(self, parquet_filename: str):
         assert os.path.exists(parquet_filename), f"Invalid filename: {parquet_filename}"
-        self.pandas_dataframe = pd.read_parquet(parquet_filename, engine='pyarrow')
+        self.pandas_dataframe = pd.read_parquet(parquet_filename, engine="pyarrow")
 
     def filter_by_user_ids(self, user_ids: List[int]):
-        self.pandas_dataframe = self.pandas_dataframe[self.pandas_dataframe.user_id.isin(user_ids)]
+        self.pandas_dataframe = self.pandas_dataframe[
+            self.pandas_dataframe.user_id.isin(user_ids)
+        ]
 
     def filter_by_image_0_uids(self, image_0_uids: List[str]):
-        self.pandas_dataframe = self.pandas_dataframe[self.pandas_dataframe.image_0_uid.isin(image_0_uids)]
+        self.pandas_dataframe = self.pandas_dataframe[
+            self.pandas_dataframe.image_0_uid.isin(image_0_uids)
+        ]
 
     def filter_by_image_1_uids(self, image_1_uids: List[str]):
-        self.pandas_dataframe = self.pandas_dataframe[self.pandas_dataframe.image_1_uid.isin(image_1_uids)]
-    
+        self.pandas_dataframe = self.pandas_dataframe[
+            self.pandas_dataframe.image_1_uid.isin(image_1_uids)
+        ]
+
     @classmethod
-    def from_url(cls, url: str, parquet_filename: str, force_redownload = False):
+    def from_url(cls, url: str, parquet_filename: str, force_redownload=False):
         if force_redownload:
             os.system(f"wget -O {parquet_filename} {url}")
         else:
@@ -47,28 +56,70 @@ class PickAPicV2Subset:
             "image_1": Image.open(io.BytesIO(row.jpg_1)),
             "label_0": row.label_0,
             "label_1": row.label_1,
-            "user_id": row.user_id
+            "user_id": row.user_id,
         }
 
-    def get_all_data_for_a_single_user(self, user_id: int) ->List[dict]:
-
+    def get_all_data_for_a_single_user(self, user_id: int) -> List[dict]:
         # Get the indices where the values are True
         bool_mask = (self.pandas_dataframe.user_id == user_id).values
         indices = np.where(bool_mask)[0]
-        assert len(indices) > 0, f"Found zero items for user_id: {user_id}, Are you sure you're providing the correct user_id?"
-        return [
-            self.__getitem__(idx) for idx in indices
-        ]
+        assert (
+            len(indices) > 0
+        ), f"Found zero items for user_id: {user_id}, Are you sure you're providing the correct user_id?"
+        return [self.__getitem__(idx) for idx in indices]
 
     def __len__(self):
         return self.pandas_dataframe.shape[0]
 
     def append(self, dataset: "PickAPicV2Subset"):
-        assert isinstance(dataset, PickAPicV2Subset), f"Expected dataset to be an instance of PickAPicV2Subset, but got: {type(dataset)}"
+        assert isinstance(
+            dataset, PickAPicV2Subset
+        ), f"Expected dataset to be an instance of PickAPicV2Subset, but got: {type(dataset)}"
         self.pandas_dataframe = pd.concat(
             [self.pandas_dataframe, dataset.pandas_dataframe]
         )
 
-    def save(self, parquet_filename: str, append = False):
+    def save(self, parquet_filename: str, append=False):
         # https://stackoverflow.com/a/73775084
         write(parquet_filename, self.pandas_dataframe, append=append)
+
+
+class UserContextDataset:
+    def __init__(self, pick_a_pic_v2_subset: PickAPicV2Subset):
+        self.pick_a_pic_v2_subset = pick_a_pic_v2_subset
+
+        ## store IDs of all users
+        self.user_ids = np.unique(
+            self.pick_a_pic_v2_subset.pandas_dataframe.user_id.values
+        )
+
+    def __getitem__(self, idx):
+        """
+        return all images and labels for a single user as a list
+        """
+        single_user_data = self.pick_a_pic_v2_subset.get_all_data_for_a_single_user(
+            user_id=self.user_ids[idx]
+        )
+
+        images = []
+        labels = []
+        pick_a_pic_v2_subset_dataset_indices = []
+
+        for item in single_user_data:
+            images.append(item["image_0"])
+            labels.append(item["label_0"])
+
+            images.append(item["image_1"])
+            labels.append(item["label_1"])
+
+            pick_a_pic_v2_subset_dataset_indices.append(item["dataset_index"])
+        data = {
+            "images": images,
+            "labels": labels,
+            "user_id": self.user_ids[idx],
+            "pick_a_pic_v2_subset_dataset_indices": pick_a_pic_v2_subset_dataset_indices,
+        }
+        return data
+
+    def __len__(self):
+        return len(self.user_ids)
