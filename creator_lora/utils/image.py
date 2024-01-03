64514956c9ee2931acf3472445b8a9aca7e35b4e
mayukh <mayukhmainak2000@gmail.com>
2024-01-03 05:26:21 -0800

    split image into 4 parts

--- creator_lora/utils/image.py
+++ creator_lora/utils/image.py
@@ -14,3 +14,31 @@ def load_pil_image(path_or_url: str):
         pil_image = PilImage.open(path_or_url)
 
     return pil_image
+
+def split_pil_image_into_quadrants(image):
+    """
+    Split a PIL image into 4 quadrants and return them as a list.
+
+    Parameters:
+    - image: PIL Image object
+
+    Returns:
+    - List of 4 PIL Image objects representing the quadrants
+    """
+    # Get the width and height of the image
+    width, height = image.size
+
+    # Calculate the coordinates for the four quadrants
+    left_upper = (0, 0, width // 2, height // 2)
+    right_upper = (width // 2, 0, width, height // 2)
+    left_lower = (0, height // 2, width // 2, height)
+    right_lower = (width // 2, height // 2, width, height)
+
+    # Crop the image to get the four quadrants
+    quadrant1 = image.crop(left_upper)
+    quadrant2 = image.crop(right_upper)
+    quadrant3 = image.crop(left_lower)
+    quadrant4 = image.crop(right_lower)
+
+    # Return the quadrants as a list
+    return [quadrant1, quadrant2, quadrant3, quadrant4]
