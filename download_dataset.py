9674ca77c739764571cf31838363d3ccade962b0
mayukh <mayukhmainak2000@gmail.com>
2024-01-04 10:20:49 -0800

    resume download to get 50k samples

--- download_dataset.py
+++ download_dataset.py
@@ -12,12 +12,19 @@ output_json_file = os.path.join(
     "data.json"
 )
 
+"""
+10k images = 1.3GB
+100k images = 13 GB
+500k images = 65 GB
+"""
+
 prepare_midjourney_dataset(
     images_folder=images_folder,
     output_json_file=output_json_file,
-    max_num_samples=10_000,
+    max_num_samples=50_000,
     clip_image_encoder_name="ViT-B/32",
     clip_device="cuda:3",
     device="cuda:2",
-    clip_similarity_threshold = 0.98
+    clip_similarity_threshold = 0.98,
+    resume_from_output_json_file="/data/mayukh/midjourney_dataset/data.json"
 )
