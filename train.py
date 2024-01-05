3372c1c8ab8a2c08d0bbf969fbaf23ee0824f50f
mayukh <mayukhmainak2000@gmail.com>
2024-01-05 04:27:23 -0800

    apply formatting

--- train.py
+++ train.py
@@ -15,30 +15,24 @@ seed_everything(0)
 
 config = load_json("config.json")
 
-images_folder = os.path.join(
-    config["dataset_root_folder"],
-    "images"
-)
+images_folder = os.path.join(config["dataset_root_folder"], "images")
 
-output_json_file = os.path.join(
-    config["dataset_root_folder"],
-    "data.json"
-)
+output_json_file = os.path.join(config["dataset_root_folder"], "data.json")
 dataset = MidJourneyDataset(
     images_folder=images_folder,
     output_json_file=output_json_file,
     image_transform=transforms.Compose(
         [
-            transforms.Resize((224,224)),
+            transforms.Resize((224, 224)),
             transforms.RandomHorizontalFlip(p=0.5),
             # transforms.RandomAffine(degrees = 5, translate=(0.1, 0.1)),
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.48145466, 0.4578275, 0.40821073],
                 std=[0.26862954, 0.26130258, 0.27577711],
-            )
+            ),
         ]
-    )
+    ),
 )
 
 print(f"Length of dataset: {len(dataset)} images")
@@ -49,7 +43,9 @@ num_train_samples = int(len(dataset) * config["train_test_split"])
 train_dataset = torch.utils.data.Subset(dataset, range(num_train_samples))
 
 # Created using indices from train_size to train_size + test_size.
-validation_dataset = torch.utils.data.Subset(dataset, range(num_train_samples, len(dataset)))
+validation_dataset = torch.utils.data.Subset(
+    dataset, range(num_train_samples, len(dataset))
+)
 
 
 # if not os.path.exists(config["sampler_weights_filename"]):
@@ -89,10 +85,7 @@ from creator_lora.image_encoders import CLIPImageEncoder
 # )
 
 model = models.resnet50(weights="DEFAULT")
-model.fc = nn.Sequential(
-    nn.Linear(2048,1),
-    nn.Sigmoid()
-)
+model.fc = nn.Sequential(nn.Linear(2048, 1), nn.Sigmoid())
 
 model.to(config["device"])
 
@@ -101,7 +94,7 @@ optimizer = torch.optim.Adam(
     #     *model.attnpool.parameters(),
     # ],
     model.parameters(),
-    lr = 1e-3,
+    lr=1e-3,
 )
 
 
@@ -112,7 +105,7 @@ def loss_function(logits, labels, prompts):
     has to do binary classification since it is give only one image per batch item.
 
     the potential solution would be an alternative architecture.
-    something that takes in a sequence images as input and then 
+    something that takes in a sequence images as input and then
 
 
     xander plot
@@ -131,16 +124,16 @@ def loss_function(logits, labels, prompts):
     for i in range(1, len(prompts)):
         assert prompts[0] == prompts[i]
 
-
-    logits = logits.reshape(1,-1)
+    logits = logits.reshape(1, -1)
     labels = labels.reshape(-1)
-    cross_entropy_label = (labels==1).nonzero()[0]
+    cross_entropy_label = (labels == 1).nonzero()[0]
 
     import random
 
     if random.random() < 0.01:
         print(logits)
     return nn.CrossEntropyLoss()(logits, cross_entropy_label)
+
 
 def validation_run(config, model, validation_dataloader, epoch: int):
     # Validation loop
@@ -152,13 +145,26 @@ def validation_run(config, model, validation_dataloader, epoch: int):
 
         for valid_batch in validation_dataloader:
             logits_valid = model.forward(valid_batch["image"].to(config["device"]))
-            valid_loss = loss_function(logits_valid, valid_batch["label"].to(config["device"]).float().unsqueeze(-1), prompts = valid_batch["prompt"])
-            
+            valid_loss = loss_function(
+                logits_valid,
+                valid_batch["label"].to(config["device"]).float().unsqueeze(-1),
+                prompts=valid_batch["prompt"],
+            )
+
             total_valid_loss += valid_loss.item()
 
             # Calculate accuracy
-            predictions = (logits_valid > config["validation_accuracy_threshold"]).float()
-            correct_predictions += (predictions == valid_batch["label"].to(config["device"]).float().unsqueeze(-1)).sum().item()
+            predictions = (
+                logits_valid > config["validation_accuracy_threshold"]
+            ).float()
+            correct_predictions += (
+                (
+                    predictions
+                    == valid_batch["label"].to(config["device"]).float().unsqueeze(-1)
+                )
+                .sum()
+                .item()
+            )
             total_samples += valid_batch["label"].size(0)
 
         # Calculate and print validation loss and accuracy
@@ -168,14 +174,14 @@ def validation_run(config, model, validation_dataloader, epoch: int):
 
         if config["wandb_log"]:
             wandb.log(
-                {
-                    "validation_loss": average_valid_loss,
-                    "validation_accuracy": accuracy
-                }
+                {"validation_loss": average_valid_loss, "validation_accuracy": accuracy}
             )
-        print(f"Epoch {epoch + 1}, Validation Loss: {average_valid_loss}, Accuracy: {accuracy}")
+        print(
+            f"Epoch {epoch + 1}, Validation Loss: {average_valid_loss}, Accuracy: {accuracy}"
+        )
 
     model.train()  # Set the model back to training mode
+
 
 def train_one_epoch(config, model, train_dataloader, loss_function, optimizer):
     total_loss = 0.0
@@ -183,59 +189,69 @@ def train_one_epoch(config, model, train_dataloader, loss_function, optimizer):
         optimizer.zero_grad()
         logits = model.forward(batch["image"].to(config["device"]))
 
-        loss = loss_function(logits, batch["label"].to(config["device"]).float().unsqueeze(-1), prompts = batch["prompt"])
+        loss = loss_function(
+            logits,
+            batch["label"].to(config["device"]).float().unsqueeze(-1),
+            prompts=batch["prompt"],
+        )
 
         loss.backward()
-        
+
         total_loss += loss.item()
-        
+
         if (batch_idx + 1) % config["num_gradient_accumulation_steps"] == 0:
             # Update the model parameters after accumulating gradients for config["num_gradient_accumulation_steps"] batches
             optimizer.step()
             optimizer.zero_grad()
-            
+
             # Print the average loss over config["num_gradient_accumulation_steps"] batches
             average_loss = total_loss / config["num_gradient_accumulation_steps"]
 
             if config["wandb_log"]:
-                wandb.log(
-                    {
-                        "training_loss": average_loss
-                    }
-                )
-                
+                wandb.log({"training_loss": average_loss})
+
             # print(f"Epoch {epoch + 1}, Batch {batch_idx + 1}, Average Loss: {average_loss}")
-            
+
             total_loss = 0.0  # Reset the total loss for the next accumulation
-            
+
     # If there are remaining batches, update the model parameters
     if batch_idx % config["num_gradient_accumulation_steps"] != 0:
         optimizer.step()
         optimizer.zero_grad()
-        
+
         # Print the average loss for the remaining batches
-        average_loss = total_loss / (batch_idx % config["num_gradient_accumulation_steps"])
+        average_loss = total_loss / (
+            batch_idx % config["num_gradient_accumulation_steps"]
+        )
 
         if config["wandb_log"]:
-            wandb.log(
-                {
-                    "training_loss": average_loss
-                }
-            )
+            wandb.log({"training_loss": average_loss})
         # print(f"Epoch {epoch + 1}, Remaining Batches, Average Loss: {average_loss}")
 
 
 if config["wandb_log"]:
-    wandb.init(
-        project="eden-creator-lora",
-        config=config
-    )
+    wandb.init(project="eden-creator-lora", config=config)
 
 for epoch in range(10):
-
     if epoch == 0:
-        validation_run(config=config, model=model, validation_dataloader=validation_dataloader, epoch = epoch)
+        validation_run(
+            config=config,
+            model=model,
+            validation_dataloader=validation_dataloader,
+            epoch=epoch,
+        )
 
-    train_one_epoch(config=config, model=model, train_dataloader=train_dataloader, loss_function=loss_function, optimizer=optimizer)
+    train_one_epoch(
+        config=config,
+        model=model,
+        train_dataloader=train_dataloader,
+        loss_function=loss_function,
+        optimizer=optimizer,
+    )
 
-    validation_run(config=config, model=model, validation_dataloader=validation_dataloader, epoch=epoch)
+    validation_run(
+        config=config,
+        model=model,
+        validation_dataloader=validation_dataloader,
+        epoch=epoch,
+    )
