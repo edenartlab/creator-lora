b389c541c5dec29b10380d5af34eecf1c5fdb940
mayukhdeb <mayukhmainak2000@gmail.com>
2024-01-18 05:53:06 -0800

    scale up to 200k images

--- download_dataset.py
+++ download_dataset.py
@@ -21,7 +21,7 @@ output_json_file = os.path.join(
 prepare_midjourney_dataset(
     images_folder=images_folder,
     output_json_file=output_json_file,
-    max_num_samples=50_000,
+    max_num_samples=200_000,
     clip_image_encoder_name="ViT-B/32",
     clip_device="cuda:3",
     device="cuda:2",
