5a8fb3ae4f44a40ad8db9b0197a32f787164da93
mayukhdeb <mayukhmainak2000@gmail.com>
2024-01-26 01:51:49 -0800

    extract 7z multivolume files

--- creator_lora/utils/files_and_folders.py
+++ creator_lora/utils/files_and_folders.py
@@ -1,4 +1,6 @@
 import os
+from py7zr import SevenZipFile
+import multivolumefile
 
 def create_new_clean_folder(folder: str):
     os.system(f"mkdir -p {folder}")
@@ -15,3 +17,13 @@ def get_filenames_in_a_folder(folder: str):
     files =  os.listdir(folder)
     files = [f'{folder}/' + x for x in files]
     return files
+
+def extract_7z_multivolume(name: str, extract_path):
+    """
+    Extracts content from a multi-volume 7z archive.
+
+    see: https://py7zr.readthedocs.io/en/stable/user_guide.html#extraction-from-multi-volume-archive
+    """
+    with multivolumefile.open(name, mode='rb') as target_archive:
+        with SevenZipFile(target_archive, 'r') as archive:
+            archive.extractall(extract_path)
