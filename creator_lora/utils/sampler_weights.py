831d37d1f015e2a7cbf50ef5a33a4b45ba347f36
mayukhdeb <mayukhmainak2000@gmail.com>
2024-01-26 06:22:28 -0800

    cast to int for float labels

--- creator_lora/utils/sampler_weights.py
+++ creator_lora/utils/sampler_weights.py
@@ -1,7 +1,9 @@
 import torch
 
 def compute_sampler_weights(labels):
-    label_counts = torch.bincount(torch.tensor(labels))
+    # manually cast to int for float labels for approximate weights
+    # I hope it would be good enough
+    label_counts = torch.bincount(torch.tensor(labels).int())
     label_weights = 1 / label_counts.float()
     weights = label_weights[labels]
     weights /= weights.sum()
