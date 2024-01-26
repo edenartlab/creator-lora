705af81e983d806ed8a56ea8672d69ec1344a9c6
mayukhdeb <mayukhmainak2000@gmail.com>
2024-01-26 04:37:12 -0800

    add column names for sanity

--- creator_lora/dataset/ava.py
+++ creator_lora/dataset/ava.py
@@ -3,6 +3,7 @@ original dataset link:
 https://academictorrents.com/details/71631f83b11d3d79d8f84efe0a7e12f0ac001460
 """
 import os
+import pandas as pd
 from ..utils.download import download_torrent
 from ..utils.files_and_folders import get_filenames_in_a_folder, extract_7z_multivolume
 
@@ -20,6 +21,43 @@ def download_ava_dataset(
         torrent_filename,
         save_path=download_folder
     )
+
+def assign_column_names(df):
+    """
+    Column 1: Index
+
+    Column 2: Image ID 
+
+    Columns 3 - 12: Counts of aesthetics ratings on a scale of 1-10. Column 3 
+    has counts of ratings of 1 and column 12 has counts of ratings of 10.
+
+    Columns 13 - 14: Semantic tag IDs. There are 66 IDs ranging from 1 to 66.
+    The file tags.txt contains the textual tag corresponding to the numerical
+    id. Each image has between 0 and 2 tags. Images with less than 2 tags have
+    a "0" in place of the missing tag(s).
+
+    Column 15: Challenge ID. The file challenges.txt contains the name of 
+    the challenge corresponding to each ID.
+    """
+    column_names = [
+        "index",
+        "image_id"
+    ]
+    column_names.extend(
+        [
+            f"num_ratings_{i}" for i in range(1, 11)
+        ]
+    )
+
+    column_names.extend(
+        [
+            f"semantic_tag_{i}" for i in range(0, 2)
+        ]
+    )
+    column_names.append("challenge_id")
+    df.columns = column_names
+
+    return df
 
 def build_ava_dataset(
     download_folder: str,
@@ -57,3 +95,6 @@ def build_ava_dataset(
         extracted_images_folder,
         "images"
     )
+    ava_df = pd.read_csv(ava_txt_file, delim_whitespace=True, header=None)
+    ava_df = assign_column_names(df = ava_df)
+    print(ava_df)
