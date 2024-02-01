81d88e06c09177513c2c690386e9972c7b514b9b
mayukhdeb <mayukhmainak2000@gmail.com>
2024-02-01 04:40:46 -0800

    formatting

--- creator_lora/dataset/pixabay.py
+++ creator_lora/dataset/pixabay.py
@@ -1,8 +1,24 @@
 import requests
 
-def pixabay_api_request(api_key, query, lang="en", image_type="all", orientation="all", category=None,
-                        min_width=0, min_height=0, colors=None, editors_choice=False, safesearch=False,
-                        order="popular", page=1, per_page=20, callback=None, pretty=False):
+
+def pixabay_api_request(
+    api_key,
+    query,
+    lang="en",
+    image_type="all",
+    orientation="all",
+    category=None,
+    min_width=0,
+    min_height=0,
+    colors=None,
+    editors_choice=False,
+    safesearch=False,
+    order="popular",
+    page=1,
+    per_page=20,
+    callback=None,
+    pretty=False,
+):
     # Pixabay API endpoint
     api_url = "https://pixabay.com/api/"
 
@@ -23,7 +39,7 @@ def pixabay_api_request(api_key, query, lang="en", image_type="all", orientation
         "page": page,
         "per_page": per_page,
         "callback": callback,
-        "pretty": "true" if pretty else "false"
+        "pretty": "true" if pretty else "false",
     }
 
     # Make the API request
