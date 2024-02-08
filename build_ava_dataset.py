09477f2db6ebbc60bb9b968d95648b05c7a4d507
mayukhdeb <mayukhmainak2000@gmail.com>
2024-02-07 22:29:38 -0800

    build ava dataset

---
+++ build_ava_dataset.py
@@ -0,0 +1,6 @@
+from creator_lora.dataset.ava import build_ava_dataset
+
+build_ava_dataset(
+    download_folder="/data/datasets/aesthetic_visual_analysis/dataset/AVA_dataset",
+    output_filename = "ava_dataset.csv"
+)
